502599c5897036e319080d4b92f23dac641dab2e
AnaLuizanc <analuizanc13@gmail.com>
2025-02-19 13:03:12 -0300

    update connection file

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
-$host="127.0.0.1";
-$port = 3306;
-$user="root";
-$password="";
-$dbname="estoque";
+    $host="127.0.0.1";
+    $port = 3306;
+    $user="root";
+    $password="";
+    $dbname="estoque";
 
-$con = mysqli_connect($host, $user, $password, $dbname, $port) or die(mysql_error());
+    $con = mysqli_connect($host, $user, $password, $dbname, $port) or die(mysql_error());
 
     if(!$con){
         echo "Falha na conexão";
